0cac7c30bda84a9318c4552d924b5d0b8ed561c8
Evan Lamenta <e_lame@encs.concordia.ca>
2018-11-08 12:50:22 -0500

    added Insert,update,and delete Item functions

--- model/database.js
+++ model/database.js
@@ -1,6 +1,4 @@
-
-
-  var mysql = require('mysql');
+var mysql = require('mysql');
 
 class Database{
   constructor(){
@@ -29,7 +27,6 @@ class Database{
     this.con.end((err) => {
       console.log("Disconnected!")
     });
-
   }
 
   insertUser(values){
@@ -39,9 +36,7 @@ class Database{
       if (err) throw err;
         console.log("1 record inserted, ID: " + result.insertId);
     });
-
     this.mydisconnect();
-
   }
 
   deleteUser(id){
@@ -49,15 +44,11 @@ class Database{
     this.con.query(
     'DELETE FROM Users WHERE id = ?', id, (err, result) => {
       if (err) throw err;
-
       console.log(`Deleted ${result.affectedRows} row(s)`);
     }
   );
-
     this.mydisconnect();
-
   }
-
 
   selectAllUsers(){
     this.myconnect();
@@ -66,12 +57,42 @@ class Database{
     console.log(result);
     return result;
   });
-
     this.mydisconnect();
-
-
 }
 
+
+
+  insertItem(values){
+    this.myconnect();
+    var sql = "INSERT INTO Item (itemType,title,author,format,pages,publisher,language,isbn10,isbn13,director, producers,actors,subtitles,dubbed,releaseDate,runTime,type,artist,label,asin  ) VALUES ?";
+    this.con.query(sql, [values], function (err, result) {
+      if (err) throw err;
+        console.log("1 record inserted, ID: " + result.insertId);
+    });
+    this.mydisconnect();
+  }
+
+
+  updateItem(values,itemid){
+    this.myconnect();
+    var sql = "UPDATE Item SET itemType = ?,title = ?,author = ?,format = ?,pages = ?,publisher = ?,language = ?,isbn10 = ?,isbn13 = ?,director = ?, producers = ?,actors = ?,subtitles = ?,dubbed = ?,releaseDate = ?,runTime = ?,type = ?,artist = ?,label = ?,asin = ? WHERE itemid = ?";
+    this.con.query(sql, [values[0][0],values[0][1],values[0][2],values[0][3],values[0][4],values[0][5],values[0][6],values[0][7],values[0][8],values[0][9],values[0][10],values[0][11],values[0][12],values[0][13],values[0][14],values[0][15],values[0][16],values[0][17],values[0][18],values[0][19],itemid], function (err, result) {
+      if (err) throw err;
+        console.log(result.affectedRows + " record(s) updated");
+    });
+    this.mydisconnect();
+  }
+
+  deleteItem(id){
+    this.myconnect();
+    this.con.query(
+    'DELETE FROM Item WHERE itemid = ?', id, (err, result) => {
+      if (err) throw err;
+      console.log(`Deleted ${result.affectedRows} row(s)`);
+    }
+  );
+    this.mydisconnect();
+  }
 
 }
 module.exports = Database;
