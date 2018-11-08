a0970f490b4e1d0f26579ef977800f416fe0623b
jiangyuhua <joeyyy1967@gmail.com>
2018-11-08 18:52:34 -0500

    modifyItem

--- routes/catalog.js
+++ routes/catalog.js
@@ -12,7 +12,7 @@ router.get('/panel/catalog',
     }
 );
 
-// Add item
+// Add item 
 router.get('/panel/catalog/add',
     function(req, res) {
             res.locals.message = req.flash('error');
@@ -51,9 +51,19 @@ router.delete('/panel/catalog/:item_id',
 );
 
 // Modify item with id: item_id
-router.put('/panel/catalog/:item_id',
+router.get('/panel/catalog/modify/:item_id',
     function(req, res) {
+        var item = Console.getItem(req.params.item_id);
+        console.log(item);
+        res.locals.message = req.flash('error');
+        res.locals.item = item;
+        if (item.itemType == 'Music') res.render('modifyMusic');
+        if (item.itemType == 'Video') res.render('modifyVideo');
+        if (item.itemType == 'Magezine') res.render('modifyMusic');
+        if (item.itemType == 'Book') res.render('modifyBook');
+        if (item.itemType == 'Movie') res.render('modifyMovie');
     }
 );
+
 
 module.exports = router;
